fe9695b63ff07e40f21197efd3fb93113d34d123
Junya245 <kusudajunya0909@icloud.com>
2025-02-27 12:24:39 +0900

    テスト

--- testPaper/middle/question4.js
+++ testPaper/middle/question4.js
@@ -24,7 +24,7 @@ export const toggleArrayElement = (array, ele) => {
     });
   }
   if (exist === false) {
-    let add = array.push(ele);
-    return add;
+    array.push(ele);
+    return array;
   }
 };
